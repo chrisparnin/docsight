010c835744cd23685b1fce813fe4cf411a14b486
Chris Parnin <chris.parnin@gmail.com>
2013-03-11 13:01:11 -0400

    New filter approach.

---
+++ js/filterpresets.js
@@ -0,0 +1,155 @@
+var filtersPresets = 
+{
+	include_patterns : 
+		[
+			"*://*.google.tld/search*",
+			"*://stackoverflow.tld/*",
+
+			"*://groups.google.com/*",
+			"*://answers.oreilly.com/*",
+
+			"*://*readthedocs.org/*",
+			"*://apidock.com/*",
+	
+			"*://developer.mozilla.org/*",
+
+			// javadocs, general
+			"*://*/docs/javadocs/*",
+
+			// repository sites?
+			//"*://code.google.com/*",
+			//"://github.com/*",
+		
+			// clojure
+			"*://clojure.org/*",
+			"*://clojure.github.com/clojure/*",	
+
+			// nodejs
+			"*://nodejs.org/api/*",
+			
+			// coffeescript
+			"*://coffeescript.org/*",
+
+			// ruby
+			"*://ruby-doc.org/*",
+			"*://www.ruby-doc.org/*",
+			"*://api.rubyonrails.org/*",
+
+			// host many projects' documentation, generted by yard.	
+			"*://rubydoc.info/*",
+			
+
+		
+			// route frameworks	
+			"*://stripe.com/docs/*",
+			"*://github.com/NancyFx/Nancy/wiki/*",
+				
+	
+			// css-ish
+			"*://compass-style.org/reference/*",
+		
+			"*://lesscss.tld/#*",
+			"*://less-ja.studiomohawk.com/*",
+			"*://ciembor.github.com/lesscss.org/*",
+			"*://bertzzie.com/post/7/dokumentasi-less-bahasa-indonesia*",
+			
+	
+			// php
+			"*://www.php.net/manual/*",
+		
+					
+			"*://laravel.com/docs/*",
+
+
+			// sql
+
+			"*://dev.mysql.com/doc/*",
+
+			"*://www.postgresql.org/docs/*",
+			"*://www.postgresql.jp/document/*",
+			"*://docs.postgresqlfr.org/*",
+			"*://wiki.postgresql.org/*",
+
+			// python
+			"*://docs.python.org/",
+			"*://wiki.python.org/",
+			"*://*python.org/dev/peps/*",
+			"*://*python.org/doc/*"
+
+			"*://docs.djangoproject.com/*",
+
+			"*://docs.scipy.org/doc/*",
+			"*://www.scipy.org/Cookbook*",
+			"*://matplotlib.org/*",
+
+			// R
+			"*://cran.r-project.org/doc/*",
+			"*://rwiki.sciviews.org/*",
+
+			// IDL
+			"*://www.exelisvis.com/docs/*",
+
+			// C++ (very incomplete)
+			"*://www.boost.org/doc/*",
+
+			// eLisp
+			"*://www.gnu.org/software/emacs/manual/html_node/elisp/*",
+			"http://www.gnu.org/software/emacs/manual/html_mono/elisp.html*",
+
+			///// javascript ///////
+			// jquery
+			// prototype
+			// d3
+			// yui
+			// dojo
+			// processing
+			// extjs
+			// mootools
+			// raphael
+			// rightjs
+			// threejs
+			// zepto
+			// shipyard
+			// knockoutjs
+			// x toolkit edge
+			// angularjs
+			// enyo
+			// underscore
+			// bonsai
+			// kineticjs
+			// thorax
+			// webapp install
+			// createjs
+			// qooxdoo
+			// fabricjs
+			// kineticjs
+			// momentjs
+
+			// msdn
+			"*://msdn.microsoft.com/*",
+			"*://code.msdn.microsoft.com/*",
+			"*://social.msdn.microsoft.com/*",
+
+			// eclipse
+			"*://wiki.eclipse.org/PDE/*",
+			"*://help.eclipse.org/*",
+
+			// java
+			"*://docs.oracle.com/javase/*/docs/*"
+		
+			// scala
+			"*://www.scala-lang.org/api/*",
+	
+			// android	
+			"*://developer.android.com/*"		
+
+			// iOS
+			"*://developer.apple.com/library/ios/*"
+	
+			// chrome
+			"*://developer.chrome.com/extensions/*"
+
+
+		]
+
+}
